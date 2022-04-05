addf1959f6398024025240664b7137506d490811
nongluck <30003774+luckloa@users.noreply.github.com>
2022-04-04 23:22:26 -0400

    submit luck's TC

--- src/test/java/pages/buyPage.java
+++ src/test/java/pages/buyPage.java
@@ -11,6 +11,12 @@ public class buyPage {
 
     @FindBy(xpath="//*[@id=\"otm-main-nav\"]/div[1]/div/nav/ul/li[2]/div/div/div[1]/ul/li[1]/a/div[2]/span")
     public WebElement propertyForSale;
+    
+    @FindBy(xpath="//a[@class='otm-more-link'][contains(text(),'Show more properties')]")
+    public WebElement showMoreProperties;
+    
+    @FindBy(id="search-location-sale")
+    public WebElement searchBox;
 
 
 }
