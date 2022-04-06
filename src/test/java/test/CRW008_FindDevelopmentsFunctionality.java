d7e9770f05d9fa5fe2907e21ce677cd752444d6b
toeinaka <95002954+toeinaka@users.noreply.github.com>
2022-04-06 15:58:04 -0400

    Newest

--- src/test/java/test/CRW008_FindDevelopmentsFunctionality.java
+++ src/test/java/test/CRW008_FindDevelopmentsFunctionality.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -16,18 +18,19 @@ import driver.Driver;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import utilities.PropertiesReader;
 import utilities.commonMethods;
 
 public class CRW008_FindDevelopmentsFunctionality extends commonMethods {
 
-	@BeforeTest
+	@BeforeMethod(alwaysRun = true)
 	public void launchBrowser() {
 		// 1. Open the browser and navigate to https://www.onthemarket.com/
 		Driver.getDriver();
 
 	}
 
-	@Test(enabled = false)
+	@Test(priority =1,enabled = true)
 	public void CRW008_FindAllDevelopersLogo() {
 
 		// 2.Click "Accept All" to accept all cookies on the pop-up box.
@@ -48,7 +51,7 @@ public class CRW008_FindDevelopmentsFunctionality extends commonMethods {
 
 	}
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void CRW008_CRW008_FindAllDevelopersLogoClickable() {
 		// 2.Click "Accept All" to accept all cookies on the pop-up box.
 		comm.acceptCookieBtn.click();
@@ -81,36 +84,38 @@ public class CRW008_FindDevelopmentsFunctionality extends commonMethods {
 		}
 	}
 
-	@Test(enabled = false) //bugs
+	@Test(enabled = true) //bugs
 	public void CRW008_VerifyFieldboxIsWorking() {
-		// 2.Click "Accept All" to accept all cookies on the pop-up box.
-		comm.acceptCookieBtn.click();
+				// 2.Click "Accept All" to accept all cookies on the pop-up box.
+				comm.acceptCookieBtn.click();
+				
+				// 3.Hover the pointer to "New Homes" tab on top of the page.
+				Actions action = new Actions(driver);
+				action.moveToElement(newHomes.newhomesmenu).build().perform();
+				
+				// 4.The "New Homes" menu should appears.
+				// 5.Click on "Find Developments" menu.
+				newHomes.findDevelopments.click();
+				
+				// 6.Put in text field in the first text field box by valid data info provided.
+				newHomes.searchBar.sendKeys(PropertiesReader.getProperty("CRW008_CityName"));
+				
+				// 7.User should see and select the correct choice.
 
-		// 3.Hover the pointer to "New Homes" tab on top of the page.
-		Actions action = new Actions(driver);
-		action.moveToElement(newHomes.newhomesmenu).build().perform();
+				// 8.Click on magnify logo in red blackground on the right of that field box.
+				Actions actions = new Actions(driver);
+				WebElement elementLocator = newHomes.searchBarButton;
+				actions.doubleClick(elementLocator).perform();
+				
 
-		// 4.The "New Homes" menu should appears.
-		// 5.Click on "Find Developments" menu.
-		newHomes.findDevelopments.click();
+				// 9.User should see developers' information listing that located in specific
+				// entered city.
+				String expectedTitle = PropertiesReader.getProperty("CRW008_BristolTitle");
+				String actaulTitle = driver.getTitle();
+				Assert.assertEquals(actaulTitle, expectedTitle);
+			}
 
-		// 6.Put in text field in the first text field box by valid data info provided.
-		Actions actions = new Actions(driver);
-		WebElement elementLocator = driver.findElement(By.id("ID"));
-		actions.doubleClick(elementLocator).perform();
-
-		// 7.User should see and select the correct choice.
-
-		// 8.Click on magnify logo in red blackground on the right of that field box.
-
-		// 9.User should see developers' information listing that located in specific
-		// entered city.
-		
-		
-
-	}
-
-	@AfterTest(enabled = false)
+	@AfterMethod(alwaysRun = true)
 	public void afterTest() {
 		Driver.tearDown();
 	}
