e8d0213ea4641c694bdd6369637c6db03cdeca99
crewXproject <101949841+crewXproject@users.noreply.github.com>
2022-04-07 16:47:52 -0400

    Update buyPage.java

--- src/test/java/pages/buyPage.java
+++ src/test/java/pages/buyPage.java
@@ -19,63 +19,54 @@ public class buyPage {
     @FindBy(id="search-location-sale")
     public WebElement searchBox;
 
-        @FindBy(xpath="//span[text()='Buy']")
-        public WebElement buyTab;
+    @FindBy(xpath="//span[text()='Buy']")
+    public WebElement buyTab;
         
-        @FindBy(xpath="//span[text()='Overseas property for sale']")
-        public WebElement overseasForSale;
+    @FindBy(xpath="//span[text()='Overseas property for sale']")
+    public WebElement overseasForSale;
         
-        @FindBy(xpath="//h3[text()='Canary Islands']")
-        public WebElement CanaryIslands;
+    @FindBy(xpath="//h3[text()='Canary Islands']")
+    public WebElement CanaryIslands;
         
-        @FindBy(xpath="//a[text()='Thailand']")
-        public WebElement Thailand;
-        
-        public void clickOverseasForSale(){
-        
-            //Instantiate Action Class        
-            Actions actions = new Actions(Driver.driver);
-            actions.moveToElement(buyTab).perform();
-            System.out.println("Done Mouse hover on 'Buy'");
-            overseasForSale.click();
-            if(Driver.driver.getCurrentUrl().contains("overseas")) {
-                System.out.println("user is able to open overseas page");
-            }else {
-                System.out.println("user not able to open overseas page");
-            }
-            
-        }
-        
-        public void clickCanaryIslands() {
-            Actions actions = new Actions(Driver.driver);
-            actions.moveToElement(buyTab).perform();
-            System.out.println("Done Mouse hover on 'Buy'");
-            overseasForSale.click();
-            CanaryIslands.click();
-            if(Driver.driver.getCurrentUrl().contains("property")) {
-                
-                System.out.println("user able to open Canary Islands page");            
-            }else {
-                
-                System.out.println("user not able to open Canary Islands page");
-            }
-            
-        }
-        
-        public void clickThailand() {
-            Actions actions = new Actions(Driver.driver);
-            actions.moveToElement(buyTab).perform();
-            System.out.println("Done Mouse hover on 'Buy'");
-            overseasForSale.click();
-            Thailand.click();
-            if(Driver.driver.getCurrentUrl().contains("Thailand")){
-                
-                System.out.println("user able to open Thailand page");
-            }else {
-                System.out.println("user not able to open Thailand page");
-            }
-            
+    @FindBy(xpath="//a[text()='Thailand']")
+    public WebElement Thailand;
+
+    public void clickOverseasForSale() {
+        //Instantiate Action Class
+        Actions actions = new Actions(Driver.driver);
+        actions.moveToElement(buyTab).perform();
+        System.out.println("Done Mouse hover on 'Buy'");
+        overseasForSale.click();
+        if (Driver.driver.getCurrentUrl().contains("overseas")) {
+            System.out.println("user is able to open overseas page");
+        } else {
+            System.out.println("user not able to open overseas page");
         }
     }
 
+    public void clickCanaryIslands() {
+        Actions actions = new Actions(Driver.driver);
+        actions.moveToElement(buyTab).perform();
+        System.out.println("Done Mouse hover on 'Buy'");
+        overseasForSale.click();
+        CanaryIslands.click();
+        if (Driver.driver.getCurrentUrl().contains("property")) {
+            System.out.println("user able to open Canary Islands page");
+        } else {
+            System.out.println("user not able to open Canary Islands page");
+        }
+    }
 
+    public void clickThailand() {
+        Actions actions = new Actions(Driver.driver);
+        actions.moveToElement(buyTab).perform();
+        System.out.println("Done Mouse hover on 'Buy'");
+        overseasForSale.click();
+        Thailand.click();
+        if (Driver.driver.getCurrentUrl().contains("Thailand")) {
+            System.out.println("user able to open Thailand page");
+        } else {
+            System.out.println("user not able to open Thailand page");
+        }
+    }
+}
