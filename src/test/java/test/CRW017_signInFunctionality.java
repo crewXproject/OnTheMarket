3bf045b1de99396f21f977d653f23055500ac62e
weerawichgithub <weerawich@gmail.com>
2022-04-08 20:45:51 -0400

    update pom.xml

--- src/test/java/test/CRW017_signInFunctionality.java
+++ src/test/java/test/CRW017_signInFunctionality.java
@@ -1,6 +1,4 @@
 package test;
-
-
 import org.testng.annotations.AfterMethod;
 
 import org.testng.annotations.BeforeMethod;
@@ -12,40 +10,32 @@ import driver.Driver;
 import utilities.PropertiesReader;
 import utilities.commonMethods;
 
+@Test (groups= {"Smoke"})
 public class CRW017_signInFunctionality extends commonMethods {
 
 	@BeforeMethod
-
 	public void openBrowser() {
-
 		Driver.getDriver();
-
 	}
 
-	@Test (groups= {"Smoke"})
-
+	@Test
 	public void signinPage() {
-
 		comm.acceptCookieBtn.click();
 		comm.signinMenu.click();
 		signin.emailInput.sendKeys(PropertiesReader.getProperty("CRW017.email"));
 		signin.nextButton.click();
 		signin.passwordInput.sendKeys(PropertiesReader.getProperty("CRW017.password"));
 		signin.signinButton.click();
-	
 	}
 
 	@Test
-
 	public void signinPageWithInvalidPassword() {
-
 		comm.acceptCookieBtn.click();
 		comm.signinMenu.click();
 		signin.emailInput.sendKeys(PropertiesReader.getProperty("CRW017.email"));
 		signin.nextButton.click();
 		signin.passwordInput.sendKeys(PropertiesReader.getProperty("CRW017.invalidpassword"));
 		signin.signinButton.click();
-
 	}
 
 	@AfterMethod
